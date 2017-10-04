0f8d7ba8df85912b6e6ed9d7dbbd393f565200c9
kanersito16 <lomelialberto@hotmail.com>
2017-10-04 09:29:34 -0600

    Added views to create/edit blog post inside the site.

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
